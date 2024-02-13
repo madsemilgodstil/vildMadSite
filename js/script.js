a0ec3b85419e54ae7b4cecd800166cc9bdc70ef8
madsemilgodstil <mads93n2@stud.kea.dk>
2024-02-13 13:45:17 +0100

    fff

--- js/script.js
+++ js/script.js
@@ -1,17 +1,7 @@
-const svgContainer = document.querySelector('.svg-container')
-
-document.addEventListener('mousemove', e => {
-  const mouseX = e.pageX / window.innerWidth - 0.5
-  const mouseY = e.pageY / window.innerHeight - 0.5
-
-  // Adjust the movement range and direction as needed
-  const translateX = mouseX * 50
-  const translateY = mouseY * 50
-
-  svgContainer.style.transform = `translate(${translateX}px, ${translateY}px)`
-})
-
+const svgContainer_1 = document.querySelector('.svg-container_1')
 const svgContainer_2 = document.querySelector('.svg-container_2')
+const svgContainer_3 = document.querySelector('.svg-container_3')
+const svgContainer_4 = document.querySelector('.svg-container_4')
 
 document.addEventListener('mousemove', e => {
   const mouseX = e.pageX / window.innerWidth - 0.5
@@ -21,11 +11,20 @@ document.addEventListener('mousemove', e => {
   const translateX = mouseX * 50
   const translateY = mouseY * 50
 
+  svgContainer_1.style.transform = `translate(${translateX}px, ${translateY}px)`
   svgContainer_2.style.transform = `translate(${translateX}px, ${translateY}px)`
+  svgContainer_3.style.transform = `translate(${translateX}px, ${translateY}px)`
+  svgContainer_4.style.transform = `translate(${translateX}px, ${translateY}px)`
 })
 
 const clicked = document.querySelector('.svg-container_1')
+const clicked_2 = document.querySelector('.svg-container_2')
+const clicked_3 = document.querySelector('.svg-container_3')
+const clicked_4 = document.querySelector('.svg-container_4')
 
 document.addEventListener('click', () => {
-  clicked.window.location.href = './html/løvskov.html'
+  clicked_1.window.location.href = './html/løvskov.html'
+  clicked_2.window.location.href = './html/løvskov.html'
+  clicked_3.window.location.href = './html/løvskov.html'
+  clicked_4.window.location.href = './html/løvskov.html'
 })
